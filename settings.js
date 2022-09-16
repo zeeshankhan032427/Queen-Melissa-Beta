ffc02aea3d8976df655bc2969607a5ae711378e1
Senu Boy <112702470+SenuBoy@users.noreply.github.com>
2022-09-16 06:23:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ global.location = "SriLanka, Kaluthara, Panadura" //ur location
 
 //bot bomdy 
 global.owner = ['94753420144']
-global.ownertag = '94753420144' //ur tag number
+global.ownertag = '' //ur tag number
 global.botname = 'Queen-Melissa-Beta' //ur bot name
 global.linkz = "https://github.com/SenuBoy/Queen-Melissa-Beta" //your theme url which will be displayed on whatsapp
 global.websitex = "https://github.com/SenuBoy/Queen-Melissa-Beta" //ur website to be displayed
